80bbeb63532ef300683cf7a6d9ba0756013d7981
alexgirardello <alexandre.girardello@estudante.ifro.edu.br>
2022-11-26 10:46:42 -0400

    bug fix x2

--- data/data.py
+++ data/data.py
@@ -2,14 +2,14 @@ import pickle
 
 
 def save_to_file(data):
-    with open('/Users/alexgirardello/PycharmProjects/ihc/data/data.pkl', 'wb') as output:
+    with open('../../data/data.pkl', 'wb') as output:
         pickle.dump(data, output, pickle.HIGHEST_PROTOCOL)
     return True
 
 
 def open_file():
     try:
-        with open('/Users/alexgirardello/PycharmProjects/ihc/data/data.pkl', 'rb') as input:
+        with open('../../data/data.pkl', 'rb') as input:
             return pickle.load(input)
     except:
         return False
